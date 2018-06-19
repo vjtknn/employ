b3106ca6ff6f08ab24aa20a0aaa839645541816d
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-06-19 18:40:47 +0200

    Post action for jobs

--- app/controllers/api/v1/jobs.rb
+++ app/controllers/api/v1/jobs.rb
@@ -1,6 +1,7 @@
 module API
   module V1
     class Jobs < Base
+
         resources :jobs do
           desc 'return all of the jobs'
           get do
@@ -14,6 +15,14 @@ module API
         end
         get '/:id' do
           Job.find(params[:id])
+        end
+
+        desc 'Create a job'
+        params do
+          requires :title, type: String, desc: 'Title for new job'
+        end
+        post do
+          Job.create!(title: params[:title])
         end
       end
     end
