fe5c76a47b6ac6c841fd67ecb8cf30b6ed9f3fc4
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-07-01 16:47:00 +0200

    Spec for google login

---
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Users::OmniauthCallbacksController, type: :controller do
+
+  before(:each) do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
+  describe 'Google' do
+
+    context 'Success handling' do
+
+      before(:each) do
+        request.env['omniauth.auth'] = FactoryBot.create(:auth_hash, :google)
+        get :google_oauth2
+      end
+
+      let(:user) { User.find_by(email: 'testuser@gmail.com') }
+
+      it 'should set current_user to proper user' do
+        expect(subject.current_user).to eq(user)
+      end
+
+    end
+
+  end
+end
