4a638ff2d6536e804023f7de8af00dfbfc0abaf6
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-07-01 16:13:41 +0200

    Fix style

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,19 +28,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable,
-         :registerable, :recoverable,
-         :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+  :registerable, :recoverable,
+  :rememberable, :trackable, :validatable,
+  :omniauthable, omniauth_providers: [:google_oauth2]
 
-         def self.from_omniauth(access_token)
-             data = access_token.info
-             user = User.where(email: data['email']).first
-             unless user
-                 user = User.create(name: data['name'],
-                    email: data['email'],
-                    password: Devise.friendly_token[0,20]
-                 )
-             end
-             user
-         end
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    user ||= User.create(name: data['name'],
+                         email: data['email'],
+                         password: Devise.friendly_token[0, 20])
+    user
+  end
 end
