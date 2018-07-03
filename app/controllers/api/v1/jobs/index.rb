68729ccee98c0941efdb3b951dd48fb58e104979
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-07-03 20:20:10 +0200

    Change desc to start with capital letter

--- app/controllers/api/v1/jobs/index.rb
+++ app/controllers/api/v1/jobs/index.rb
@@ -2,7 +2,7 @@ module API
   module V1
     module Jobs
       class Index < Base
-        desc 'return all of the jobs'
+        desc 'Return all of the jobs'
         paginate per_page: 10, offset: false
         get do
           paginate(Job.all)
